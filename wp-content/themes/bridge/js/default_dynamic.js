aa7ee9c9768f4d18039711047b963df4ce4b56aa
Leu <tuanh@Leus-MacBook-Pro.local>
2020-11-13 15:34:13 +0700

    Can ho

--- wp-content/themes/bridge/js/default_dynamic.js
+++ wp-content/themes/bridge/js/default_dynamic.js
@@ -153,8 +153,8 @@ function showContactMap() {
 }
 
 var no_ajax_pages = [];
-var qode_root = 'http://vinhomes-smart-city.vn/';
-var theme_root = 'http://vinhomes-smart-city.vn/wp-content/themes/bridge/';
+var qode_root = '/';
+var theme_root = '/wp-content/themes/bridge/';
 var header_style_admin = "";
 if(typeof no_ajax_obj !== 'undefined') {
 	no_ajax_pages = no_ajax_obj.no_ajax_pages;
